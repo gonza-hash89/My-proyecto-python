d1ab916741c824eede519646aabd23694e3ee1df
Gonzalo Pariona Maza <gonzaloparionamaza@gmail.com>
2026-07-03 21:53:23 -0500

    Create brain package directory

---
+++ jarvis/brain/__init__.py
@@ -0,0 +1,8 @@
+"""
+brain package - El cerebro de Jarvis
+
+Módulos principales:
+- memory.py: Sistema de memoria (short + long term)
+- intent.py: Reconocedor de intenciones (próximamente)
+- decision.py: Motor de decisiones (próximamente)
+"""
